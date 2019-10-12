650a8f492264d64c1ae12385c52c79532d55ccf8
Volodimir Martuniyk <volodimir.martuniyk@gmail.com>
2019-10-12 20:20:29 +0300

    Modal windows with jquery

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 $(function() {
+    
+    
+    /*Filter
+    ==================================*/
    let = filter = $("[data-filter]");
     filter.on('click',function(event){
         event.preventDefault();
@@ -18,4 +22,53 @@ $(function() {
               });  
         }
     });
+    /*Modal
+    ==================================*/
+    
+    const modalCall = $("[data-modal]");
+    const modalClose = $("[data-close]");
+    
+    modalCall.on('click', function(event){
+        event.preventDefault();
+        let $this = $(this);
+        let modalId = $this.data('modal');
+        $(modalId).addClass("show");
+        $("body").addClass("no-scroll");
+        setTimeout(function(){
+             $(modalId).find(".modal__dialog").css({
+            transform: "rotateX(0deg)"
+             });
+        },200);
+        
+    })
+      modalClose.on('click', function(event){
+        event.preventDefault();
+        let $this = $(this);
+        let modalParent = $this.parents('.modal');
+        modalParent.find(".modal__dialog").css({
+            transform: "rotateX(90deg)"
+             });  
+        setTimeout(function(){
+            modalParent.removeClass("show");
+            $("body").removeClass("no-scroll");  
+        },200); 
+    })
+    
+     $(".modal").on('click', function(event){
+         let $this = $(this);
+         $this.find(".modal__dialog").css({
+            transform: "rotateX(90deg)"
+             });  
+          setTimeout(function(){
+             $this.removeClass("show");
+            $("body").removeClass("no-scroll");
+        },200); 
+       
+    })
+      $(".modal__dialog").on('click', function(event){
+        event.stopPropagation();
+      })
+    
+    
+    
 });
